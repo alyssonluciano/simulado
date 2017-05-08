ee6842e1ae966a86dad4209eb7e99876eeee6577
Alysson <Alysson@cpd04-PC>
2017-05-08 16:47:48 -0300

    Primeiro Commit conferencia

---
+++ src/br/com/cpd/dal/ModuloConexao.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cpd.dal;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alysson
+ */
+public class ModuloConexao {
+
+    public static Connection conector() {
+
+        java.sql.Connection conexao = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/dbconferencia";
+        String user = "root";
+        String senha = "";
+
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, senha);
+            return conexao;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+
+    }
+}
